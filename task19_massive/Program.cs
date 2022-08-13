0cb524a442faf62c39e8588e0728b19bbe464f4d
Margarita <margarita27011991@gmail.com>
2022-08-14 00:33:42 +0300

    add task19_massive

--- task19_massive/Program.cs
+++ task19_massive/Program.cs
@@ -3,12 +3,6 @@ Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 string numberText = Convert.ToString(N);
 int[] array = {numberText[0],numberText[1],numberText[2],numberText[3],numberText[4]};
-//int[] array = new int[5];
-//array[0] = numberText[0];
-//array[1] = numberText[1];
-//array[2] = numberText[2];
-//array[3] = numberText[3];
-//array[4] = numberText[4];
 if (numberText.Length == 5)
     if (array[0] == array[4] && array[1] == array[3])
     {
