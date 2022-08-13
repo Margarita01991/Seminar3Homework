bed3c36a54401b7e31c8b639e908c2edbfed9519
Margarita <margarita27011991@gmail.com>
2022-08-14 00:20:57 +0300

    add task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход пятизначное число и проверяет является ли оно палиндромом 14212 -> нет, 23432 -> да
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if (numberText.Length == 5)
+    if (numberText[0] == numberText[4] && numberText[1] == numberText[3])
+{
+        Console.WriteLine("Да");
+}
+    else
+{
+        Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine("Введите неотрицательное пятизначное число");
+}
